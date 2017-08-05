bafd5b354bbde1e42a7cbdb7d1296efc7c5125e1
Max Ying <maximilian.ying@gmail.com>
2017-08-04 21:04:18 -0400

    ConsoleDisplay done

--- src/main/scala/chess/ui/ConsoleDisplay.scala
+++ src/main/scala/chess/ui/ConsoleDisplay.scala
@@ -5,39 +5,28 @@ import chess.logic._
 
 object ConsoleDisplay extends Display {
   def piece2uni(p: Piece): Char = p match {
-    case King(White, _) => '♔'
-    case King(Black, _) => '♚'
+    case King(White, _, _) => '♔'
+    case King(Black, _, _) => '♚'
     case Queen(White, _) => '♕'
     case Queen(Black, _) => '♛'
     case Bishop(White, _) => '♗'
     case Bishop(Black, _) => '♝'
     case Knight(White, _) => '♘'
     case Knight(Black, _) => '♞'
-    case Rook(White, _) => '♖'
-    case Rook(Black, _) => '♜'
-    case Pawn(White, _) => '♙'
-    case Pawn(Black, _) => '♟'
+    case Rook(White, _, _) => '♖'
+    case Rook(Black, _, _) => '♜'
+    case Pawn(White, _, _) => '♙'
+    case Pawn(Black, _, _) => '♟'
   }
 
-  type Block = List[List[Char]]
+  type Square = List[List[Char]]
 
-  def createSquare(center: Char, radius: Int, color: Color): Block = {
-    require(radius > 0)
-    val edgeRow = ("-" * (2 * radius + 3)).toList
-    val fill = color match { case White => " "; case Black => "█"}
-    val fillRow = s"|${fill * (2 * radius + 1)}|".toList
-    val centerRow = s"|${fill * radius}$center${fill * radius}|".toList
+  def createSquare(center: Char): Square = List(
+    List('-', '-'),
+    List('|', center)
+  )
 
-    val fillBand = List.fill(radius)(fillRow)
-
-    edgeRow +:
-      fillBand ++:
-      centerRow +:
-      fillBand :+
-      edgeRow
-  }
-
-  def createSuperRow(squares: List[Block]): Block = {
+  def createSuperRow(squares: List[Square]): Square = {
     squares.transpose.map(_.flatten)
   }
 
@@ -45,35 +34,35 @@ object ConsoleDisplay extends Display {
     val coords: Seq[Coord] = for (row <- 1 to 8; col <- 1 to 8) yield (row, col)
 
     val allSymbols: Seq[Char] = coords.view
-      .map(board.occupied.get)          // does the square have a piece?
+      .map(board.self.get)              // does the square have a piece?
       .map(opt => opt.map(piece2uni))   // if so, map to the character
       .map(_.getOrElse(' '))            // otherwise, just use a blank space
       .force
 
-    def colorOf(coord: Coord): Color = {
-      if ((coord._1 + coord._2) % 2 == 0) Black
-      else White
-    }
+    val allSquares: Seq[Square] = allSymbols.map(createSquare)
 
-    val coordColors = coords.map(colorOf)
-
-    val allSquares: Seq[Block] = allSymbols.zip(coordColors).map {
-      case (chr, color) => createSquare(chr, 1, color)
-    }
-
-    val superRows: List[Block] = allSquares.toList
+    val superRows: List[List[List[Char]]] = allSquares.toList
       .grouped(8)           // 8 blocks make a row
       .map(createSuperRow)  // horizontally concat them
       .toList               // iterator to list
       .reverse              // last row of chess board is printed first
 
-    // ~Technically~ not ASCII, but whatevs
-    val superRowsAscii = superRows.map {
-      _.map(_.mkString)   // each subrow in a superrow gets concatted (List[Char] -> String
-       .mkString("\n")    // subrows make the superrow with newlines
-    }
+    // The center of every 3 fine-rows is the row of "squares".
+    // Above and below is just line padding
+    val fineRows: List[List[Char]] = superRows.flatten
 
-    superRowsAscii.mkString("\n")
+    val rowEndings: Stream[Char] = Stream.continually(List('-', '|')).flatten
+
+    val withEndings: List[List[Char]] = fineRows
+      .zip(rowEndings)
+      .map { case (finerow, ending) => finerow :+ ending }
+
+    // ~Technically~ not ASCII, but whatevs
+    val asciiBoard: String = withEndings.map(_.mkString).mkString("\n")
+
+    val footer = "\n" + "-" * 17
+
+    asciiBoard + footer
   }
 
   override def displayBoard(board: Board): Unit = {
